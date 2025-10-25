05bc49ed45ecc82825aa07c8481ef8a1ae5bb647
Musa2004-me <musaelshenawy5@gmail.com>
2025-10-25 23:10:35 +0300

    Delete build directory

--- build/carta/CMakeFiles/carta_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/carta_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/carta_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
